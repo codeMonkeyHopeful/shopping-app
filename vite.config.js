5c90fca304e45908617f4f64e09e99c708a8e6b7
Ryan Jasinski <jasinski.17@gmail.com>
2025-06-22 13:59:26 -0700

    update port

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  server: {
+    port: 3500,
+  },
   plugins: [
     react(),
     VitePWA({
