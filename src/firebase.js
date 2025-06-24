0054022003516f722cd574e0c1c97881e917f61e
Ryan Jasinski <jasinski.17@gmail.com>
2025-06-24 19:59:23 +0000

    auth check working

--- src/firebase.js
+++ src/firebase.js
@@ -14,23 +14,60 @@ import {
   orderBy,
 } from "firebase/firestore";
 
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
+
 // Replace this config with your own Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyC0VyiMXqIt7XiaZLmeCsW6wF60FJZVRQQ",
-
   authDomain: "shopping-list-9c729.firebaseapp.com",
-
   projectId: "shopping-list-9c729",
-
   storageBucket: "shopping-list-9c729.firebasestorage.app",
-
   messagingSenderId: "386050954721",
-
   appId: "1:386050954721:web:cc431855e52ec5e3616ea0",
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+
+const auth = getAuth(app);
+
+// Login function using env vars for dev/test
+export async function login() {
+  const email = import.meta.env.VITE_TEST_USER_EMAIL;
+  const password = import.meta.env.VITE_TEST_USER_PASSWORD;
+
+  if (!email || !password) {
+    console.warn("No test credentials set; skipping login.");
+    return;
+  }
+
+  try {
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    console.log("Logged in UID:", userCredential.user.uid);
+  } catch (error) {
+    console.error("Login failed", error);
+  }
+}
+
+// Used for dev level login with user credentials for a locked doen firestore table
+await login();
+
+// You can also listen for auth state changes anywhere in your app:
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    console.log("User signed in:", user.uid);
+  } else {
+    console.log("No user signed in");
+  }
+});
 
 export const removeItemFirebase = (id) => {
   return deleteDoc(doc(db, "items", id))
@@ -43,10 +80,27 @@ export const removeItemFirebase = (id) => {
     });
 };
 
+const getSameItems = (currItem) => {
+  const q = query(collection(db, "items"), where("name", "==", currItem.name));
+  db.collection("items")
+    .where("name", "==", currItem.name)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        currItem.qty += doc.qty;
+      }
+    });
+  if (q.ex) {
+    // TODO: FINISH THIS
+    return 1;
+  }
+};
+
 export const addItemFirebase = (item) => {
   // item comes in withupdated properties
   // TODO: Validate item properties before adding
   // TODO: If already exists combine, dont overwrite
+  const existingItems = getSameItems();
 
   // TODO Parse properly once inputs exist and add the server time to keep all timelines the same
   return addDoc(collection(db, "items"), { ...item, upated: serverTimestamp() })
