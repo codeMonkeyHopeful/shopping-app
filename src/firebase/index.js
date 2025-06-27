796c0544c18c11c7bc279c0e07befca6970255bb
Ryan Jasinski <jasinski.17@gmail.com>
2025-06-27 22:35:46 +0000

    move firebase connects to server

--- src/firebase/index.js
+++
@@ -1,9 +0,0 @@
-// export { loginDevUser } from "./firebaseLogin";
-// export { db } from "./firebaseClient";
-// export {
-//   getItemsFirebase,
-//   removeItemFirebase,
-//   addItemFirebase,
-//   updateItemFirebase,
-//   getItemsByCategoryFirebase,
-// } from "./firebaseAPI";
